d2f40489d19551d3f641e42b0e8c85f2d8934583
TyrenK <bizinottoluca@gmail.com>
2025-03-26 10:16:03 -0300

    salvando

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from "react-native";
+import Title from './src/components/Title';
+import FormIMC from './src/components/FormIMC';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Title />
+      <FormIMC />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flex:1,
     justifyContent: 'center',
+    padding: 16,
+    backgroundColor: '#fff',
   },
 });

44cf5d612c7b35d5f6260a7c02d843b8d11c82fe
TyrenK <bizinottoluca@gmail.com>
2025-04-07 22:19:55 -0300

    Add files via upload

---
+++ ClassIMC.js
@@ -0,0 +1,24 @@
+import { Text, StyleSheet, View } from 'react-native';
+import React from 'react';
+
+//Função que exibe a classificação
+const ClassIMC = ({ classific }) => {
+    return (
+        <View style={styles.formResult}>
+            <Text style={styles.classimc}>Sua classificacão é: {classific}</Text>
+        </View>
+    );
+};
+
+
+const styles = StyleSheet.create({
+    classimc: {
+        marginTop: 20,
+        Fontsize: 24,
+        textAlign: 'left',
+        color: '#333',
+        paddingLeft: 10,
+    },
+});
+
+export default ClassIMC;
